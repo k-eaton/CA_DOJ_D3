f5d0319849c365691caba7c0f7b10112cf827fbe
k-eaton <katrina.eaton@gmail.com>
2016-08-23 10:00:16 -0700

    test database import for arrest data

---
+++ db/migrate/20160822222928_create_data.rb
@@ -0,0 +1,15 @@
+class CreateData < ActiveRecord::Migration[5.0]
+  def change
+    create_table :data do |t|
+      
+      # t.integer :given_id
+      t.integer :year
+      t.string :offense_level
+      t.string :gender
+      t.string :age_group
+      t.string :race
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'    
+
+csv_text = File.read('data/test.csv')
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|
+  non_id_portion = row.to_hash.slice(1..5)
+  Datum.create!(non_id_portion)
+end
