fc52cdb7eac23dfd43b331c3e477622b1361a641
k-eaton <katrina.eaton@gmail.com>
2016-09-13 08:32:06 -0700

    AJAX for race data

--- app/controllers/death_controller.rb
+++ app/controllers/death_controller.rb
@@ -4,8 +4,19 @@ class DeathController < ApplicationController
   end
 
   def race_data
+    race_data = {1 => "test"}
+
     respond_to do |format|
-      format.json {}
+      format.json {
+        render :json => race_data
+      }
     end
+
+
+    # @deaths.each do |death|
+    #   race_data[death.id] = death.race
+    # end
+    # content_type :json
+    # race_data.to_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # get 'data/index'
-  get 'death/index'
+  resources :death do
+    collection do
+      get 'race_data'
+    end
+  end
   root 'death#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
